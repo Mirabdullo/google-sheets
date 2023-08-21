ff425ec2eb09ff8f431844749cd95919246ce624
Mahmudov Mirabdullo <mmirabdullo1999@gmail.com>
2023-08-21 12:14:42 +0500

    change database

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,33 +1,35 @@
 const { logging } = require("googleapis/build/src/apis/logging");
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
+const path = require("path");
 
-const sequelize = new Sequelize({
-  database: 'wood',
-  username: 'postgres',
-  password: 'postgres',
-  host: '64.226.90.160',
-  port: 5432,
-  dialect: 'postgres',
-  logging: false
-});
-
-
-
-// const serverCa = [fs.readFileSync("/home/mirabdulloh/ca-certificate.crt", "utf8")];
-
-// const sequelize = new Sequelize("woodlinecrm", "doadmin", "AVNS_Hq7s9CF7p0HNn1ikIoZ", {
-//     host: "db-postgresql-fra1-95213-do-user-12466147-0.b.db.ondigitalocean.com",
-//     port: 25060,
-//     dialect: "postgres", // or 'mysql', 'sqlite', 'mssql', etc.
-//     logging: false,
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: true,
-//             ca: serverCa,
-//         },
-//     },
+// const sequelize = new Sequelize({
+//   database: 'wood',
+//   username: 'postgres',
+//   password: 'postgres',
+//   host: '64.226.90.160',
+//   port: 5432,
+//   dialect: 'postgres',
+//   logging: false
 // });
+
+
+const file = path.join(__dirname, "ca-certificate.crt")
+
+const serverCa = [fs.readFileSync(file, "utf8")];
+
+const sequelize = new Sequelize("woodlinecrm", "doadmin", "AVNS_Hq7s9CF7p0HNn1ikIoZ", {
+    host: "db-postgresql-fra1-95213-do-user-12466147-0.b.db.ondigitalocean.com",
+    port: 25060,
+    dialect: "postgres", // or 'mysql', 'sqlite', 'mssql', etc.
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            ca: serverCa,
+        },
+    },
+});
 
 // const sequelize = new Sequelize({
 //   password: "ffffffff",
